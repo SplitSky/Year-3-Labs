ae627f5e477a6c00ec8a35162cbfa52705a935ee
SplitSky <opop.neska60@gmail.com>
2021-10-12 10:36:13 +0100

    Added reading in data along with the text files

--- Alpha Particle/analysis.py
+++ Alpha Particle/analysis.py
@@ -7,11 +7,14 @@ class Data():
         self.x = []
         self.y = []
         self.filename = filename
+
     # end def
 
     def plotCustom(self):
         # get data
-        plt.plot(self.x, self.y, "b+")
+        print(self.x)
+        print(self.y)
+        plt.plot(np.array(self.x), np.array(self.y), "b+")
 
     # end def
 
@@ -24,39 +27,42 @@ class Data():
     def readData(self):
         array = [[], [], []]
         channel = []
-        voltage = []
+        pressure = []
         err = []
 
         # file open
         file = open(self.filename, "r")
         content = file.readlines()
         temp2 = 0
-        divider = len(content)/3
-        for counter in range(0,len(content)):
+        divider = len(content) / 3
+        print("divider" + str(divider))
+        for counter in range(0, len(content)):
             temp = content[counter]
             if (counter >= 0) and (counter < divider):
-                #x values
-                print(content[counter])
+                # x values
                 channel.append(float(content[counter]))
-            elif (counter >= divider) and (counter < divider*2):
-                #y values
-                voltage.append(float(content[counter]))
-            elif (counter >= divider*2) and (counter <= divider*3):
-                #error values
+            elif (counter >= divider) and (counter < divider * 2):
+                # y values
+                pressure.append(float(content[counter]))
+            elif (counter >= divider * 2) and (counter <= divider * 3):
+                # error values
                 err.append(float(content[counter]))
             else:
                 # error control
                 print("Error entry")
 
-
         self.x = channel
-        self.y = voltage
+        self.y = pressure
         self.err = err
         # Data loaded
         print("Data loaded.")
         file.close()
-        print(self.x[0])
-    #end def
+        print("x values")
+        print(self.x)
+        print(self.y)
+        print(self.err)
+
+    # end def
 
     def cubicFitData(self, title, xAxisTitle, yAxisTitle, label):
         x = self.x
@@ -81,23 +87,24 @@ class Data():
         axes_2.set_ylabel(yAxisTitle)
         axes_2.errorbar(x, y - y_fitted, yerr=y_errors, fmt='b+')
 
-        plt.savefig(title + ".png")
+        # plt.savefig(title + ".png")
         print(fit_parameters[0])
         print(fit_parameters[1])
         print(fit_parameters[2])
         print(fit_parameters[3])
         # a + bx + cx^2 + dx^3 = y
 
-    def returnDifferential(self,b,c,d,x):
+    def returnDifferential(self, b, c, d, x):
         temp = np.array(x)
-        return b+ 2 * c * temp + 3 * d * temp^2 # return a numpy array
-
-
+        return b + 2 * c * temp + 3 * d * temp ^ 2  # return a numpy array
 
     ### integrator
-
     ### iteration fitter???
 
+    def gasAnalysis(self):
+        # convert pressure into distance
+        distance = 142.3 * (np.array(self.x)/1000) # mm
+        self.x = distance #now numpy array
 
 def main():
     # 1. load the data for the selected sample
@@ -105,9 +112,43 @@ def main():
     # 3. plot the differential against energy - Bragg curve
     # 4. integrate under the curve using an integrator
     # 5.
-    d = Data("data.txt")
-    d.readData()
-    d.cubicFitData("testing", "x-axis", "y-axis", "line")
+
+    filenames = ["data.txt", "Helium.txt", "Argon.txt", "Nitrogen.txt"]
+    # Gas analysis
+
+    print("Which data set?")
+    print("1. Calibration for gas")
+    print("2. Helium")
+    print("3. Argon")
+    print("4. Nitrogen")
+    print("5. Helium 2 electric boogaloo")
+    indicator = True
+    choice = 0
+    while indicator:
+        try:
+            indicator = False
+            choice = int(input("Choice: ")) # choice is index+1
+        except:
+            print("Try again")
+
+    if choice in [2, 3, 4, 5]:
+        # gas analysis
+        print(filenames[choice - 1])
+        d = Data(filenames[choice - 1])
+        d.readData()
+        print("Gas Analysis")
+        d.plotCustom()
+    elif (choice == 0):
+        # gas calibration
+        d = Data(filenames[choice - 1])
+        d.readData()
+        print("Calibration Analysis")
+    elif (choice > 5):  # material analysis
+        d = Data(filenames[choice - 1])
+        print("Metal Analysis")
+    else:
+        print("error")
+
 
 main()
 plt.show()
